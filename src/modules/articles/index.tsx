37f701ee21df8a61f9f9b726701b3876e078d778
dellamora <francielle@dellamora.dev>
2022-10-19 20:58:25 -0300

    feat(article) added new component

---
+++ src/modules/articles/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+import { Section, Title } from "../../common/components";
+
+const Articles: React.FC = (): JSX.Element => {
+  return (
+    <Section bgColor="bg-[#FBFBFB]" id="blog">
+      <Title subTitle="Personal Blog">MY ARTICLES</Title>
+    </Section>
+  );
+};
+
+export default Articles;
