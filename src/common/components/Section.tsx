f94962eea68c70680998eb9c9eb2f958fadc800e
dellamora <francielle@dellamora.dev>
2022-10-19 20:53:01 -0300

    feat(section) added new component

---
+++ src/common/components/Section.tsx
@@ -0,0 +1,33 @@
+/* import { Container } from "./styles"; */
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+type Props = {
+  id: string;
+  children: JSX.Element | JSX.Element[];
+  bgColor?: `bg-${string}`;
+  setIsInView?: (isInView: boolean) => void;
+};
+
+export const Section = ({
+  id,
+  children,
+  bgColor,
+  setIsInView,
+}: Props): JSX.Element => {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  useEffect(() => {
+    if (setIsInView) {
+      setIsInView(inView);
+    }
+  }, [inView]);
+
+  return (
+    <section className={`relative  p-[65px] h-screen ${bgColor}`} ref={ref}>
+      <div id={id} className="absolute -top-[100px]"></div>
+      {children}
+    </section>
+  );
+};
