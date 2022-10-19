b3e24690adf13f5c11908226d1ec79b3cb1073dd
dellamora <francielle@dellamora.dev>
2022-10-19 20:52:16 -0300

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,19 @@
 import { useContext } from "react";
-import Articles from "../common/components/articles";
-import Career from "../common/components/career";
-import Contact from "../common/components/contact";
-import Footer from "../common/components/footer";
-import Header from "../common/components/header";
-import Services from "../common/components/services";
-import Skills from "../common/components/skills";
-import Works from "../common/components/works";
+import Articles from "../modules/articles";
+import Career from "../modules/career";
+import Contact from "../modules/contact";
+import Footer from "../modules/footer";
+import Hero from "../modules/hero";
+import Skills from "../modules/skills";
+import Works from "../modules/works";
 import AppContext from "../common/context/appContext";
 
 const Home = (): JSX.Element => {
   const ctx = useContext(AppContext);
+
   return (
     <>
-      <Header />
-      <Services />
+      <Hero />
       <Works />
       <Career />
       <Articles />
