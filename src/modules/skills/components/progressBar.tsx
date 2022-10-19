7a1e32bee1866e0f344b09a0c90f50e92857b318
dellamora <francielle@dellamora.dev>
2022-10-19 20:51:47 -0300

    feat(progressbar) added new component

---
+++ src/modules/skills/components/progressBar.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+import { motion } from "framer-motion";
+
+type Props = {
+  width?: `w-${string}`;
+};
+const ProgressBar = ({ width }: Props): JSX.Element => {
+  const variants = {
+    animate: { scaleX: 1 },
+    default: { scaleX: 0 },
+  };
+  return (
+    <>
+      <motion.div
+        className="h-3 rounded w-full mb-5 bg-[#656d722d] origin-[0%] "
+        transition={{ duration: 1, type: "spring", bounce: 0.25 }}
+        variants={variants}
+      >
+        <motion.div
+          className={`h-full rounded ${width} mb-10 bg-[#656D72] origin-[0%]`}
+          transition={{ delay: 0.4, duration: 1, type: "spring", bounce: 0.25 }}
+          variants={variants}
+        />
+      </motion.div>
+    </>
+  );
+};
+
+export default ProgressBar;
