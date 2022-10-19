abd561136d8307b88cf32ce2d06283f33c312ca4
dellamora <francielle@dellamora.dev>
2022-10-19 20:58:13 -0300

    refactor base layout

--- src/common/layouts/baseLayout.tsx
+++ src/common/layouts/baseLayout.tsx
@@ -1,19 +1,88 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
-import React from "react";
-import Nav from "../components/nav";
+import React, { useState, useEffect } from "react";
+import Nav from "../../modules/nav";
+import classNames from "classnames";
 
 /* import { Container } from "./styles"; */
+const isMobile = () => {
+  const ua = navigator.userAgent;
+  return /Android|Mobi/i.test(ua);
+};
 
 const BaseLayout = ({
   children,
 }: {
   children: React.ReactNode;
 }): JSX.Element => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [clicked, setClicked] = useState(false);
+  const [linkHovered, setLinkHovered] = React.useState(false);
+  const [hidden, setHidden] = useState(false);
+
+  const addEventListeners = React.useCallback(() => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseenter", onMouseEnter);
+    document.addEventListener("mouseleave", onMouseLeave);
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mouseup", onMouseUp);
+  }, []);
+
+  const removeEventListeners = React.useCallback(() => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseenter", onMouseEnter);
+    document.removeEventListener("mouseleave", onMouseLeave);
+    document.removeEventListener("mousedown", onMouseDown);
+    document.removeEventListener("mouseup", onMouseUp);
+  }, []);
+
+  useEffect(() => {
+    addEventListeners();
+    handleLinkHoverEvents();
+    return () => removeEventListeners();
+  }, [addEventListeners, removeEventListeners]);
+
+  const onMouseMove = e => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const onMouseDown = () => {
+    setClicked(true);
+  };
+
+  const onMouseUp = () => {
+    setClicked(false);
+  };
+
+  const onMouseLeave = () => {
+    setHidden(true);
+  };
+
+  const onMouseEnter = () => {
+    setHidden(false);
+  };
+
+  const handleLinkHoverEvents = () => {
+    document.querySelectorAll("a,  button, .bg-inverse").forEach(el => {
+      el.addEventListener("mouseover", () => setLinkHovered(true));
+      el.addEventListener("mouseout", () => setLinkHovered(false));
+    });
+  };
+
+  const cursorClasses = classNames("cursor", {
+    "cursor--clicked": clicked,
+    "cursor--hidden": hidden,
+    "cursor--link-hovered": linkHovered,
+  });
+
+  if (typeof navigator !== "undefined" && isMobile()) return null;
   return (
-    <div className="h-screen flex flex-col  bg-[#FFFFFF]">
+    <div className="cursor-none bg-[#FFFFFF]">
+      <div
+        className={cursorClasses}
+        style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      />
       <Nav />
-      <div className="grow  ">{children}</div>
+      {children}
     </div>
   );
 };
