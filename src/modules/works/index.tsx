d8fa150a0827117369866a1930db61080da2f707
dellamora <francielle@dellamora.dev>
2022-10-19 20:56:14 -0300

    feat(works) added new section

---
+++ src/modules/works/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+import { Section, Title } from "../../common/components";
+
+const Works: React.FC = (): JSX.Element => {
+  return (
+    <Section bgColor="bg-[#FBFBFB]" id="portfolio">
+      <Title subTitle="Featured Portfolios">MY Projects</Title>
+    </Section>
+  );
+};
+
+export default Works;
